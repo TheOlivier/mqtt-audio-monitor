5193b32711cc303e2dd2dba3e97ba7eb5f10a9cd
OliverS <odfs2005@gmail.com>
2023-10-12 13:46:38 +0200

    fix

--- MQTT Opg/sketch.js
+++ MQTT Opg/sketch.js
@@ -1,10 +1,9 @@
-//client er den variabel der bruges til at oprette forbindelse til mqtt serveren
 let client 
 //connectionDiv peger på et DIV element i HTML siden 
 let connectionDiv
 var vol;
 
-let columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let columns = 10;
 let colPos = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900]
 
 //setup er den funktion der kører, før selve web-appen starter 
@@ -13,7 +12,7 @@ function setup() {
   createCanvas(1000, 600);
   //tag fat i en div i HTML dokumentet - med id "connection"
   connectionDiv = select('#connection')
-  
+
   //forsøg at oprette forbindelse til MQTT serveren 
   client = mqtt.connect('wss://mqtt.nextservices.dk')
 
@@ -22,10 +21,10 @@ function setup() {
     console.log('Client connected: ', m)
     connectionDiv.html('You are now connected to mqtt.nextservices.dk')
   })
-  
+
   //subscribe på emnet 'programmering'
   client.subscribe('viking/lyd')
-  
+
   //når vi modtager beskeder fra MQTT serveren kaldes denne funktion
   client.on('message', (topic, message) => {
     //console.log('Received Message: ' + message.toString())
@@ -35,17 +34,27 @@ function setup() {
 
     //Sæt beskeden ind på hjemmesiden 
     connectionDiv.html('Received message: <b>' + message + '</b> on topic: <b>' + topic + '</b>')
-  })  
-
+  })
+    frameRate(5);
 }
 
+let curCol = 0;
+let drawBuf = []
 function draw(){
   background(0);
   fill(200);
   stroke(1);
+    if (drawBuf.length == columns)
+    {
+        drawBuf.shift();
+    }
 
-  for(let i = 0; i < columns.length; i++){
-    rect(colPos[i], height , 100, -vol/2);
+  drawBuf.push(-vol/2);
+    print(drawBuf.length)
+  for(let i = 0; i < drawBuf.length; i++){
+    rect(colPos[i], height , 100, drawBuf[i]);
   }
-  //rect(0, height , 100, -vol/2);
+    curCol++;
+
+    
 }
